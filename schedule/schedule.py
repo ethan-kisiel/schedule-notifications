20ba378d079ca1653a2369d0cb5423a1c00b3c9f
Ethan Kisiel <ethan.a.kisiel@gmail.com>
2022-01-31 17:17:08 -0500

    Added main.py and added send mesage, compare time, and get weekday functionality to schedule.Schedule

--- schedule/schedule.py
+++ schedule/schedule.py
@@ -1,13 +1,14 @@
 from numpy import array
 import openpyxl, smtplib, ssl
 from datetime import datetime
-
+from email.message import EmailMessage
 class Schedule:
     '''
     '''
     def __init__(self, schedule: str, limits: array):
         self.schedule = schedule
         self.schedule_data = self.get_data(*limits)
+        self.days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
         
     def get_data(self, title_row: int, title_col, min_col: int, max_col: int, min_row: int, max_row: int):
         '''
@@ -43,13 +44,27 @@ class Schedule:
             return(f'{time[0]}:{time[1]}:PM')
         else:
             return(f'{time[0]}:{time[1]}:AM')
-    
+
     def compare_time(self, time: str, buffer: int):
         '''
         Takes time in the format of HH:MM:AM/PM
         returns true if time is within buffer(minutes) of current time
         '''
-
+        current_time = datetime.now()
+        current_time = self.convert_time(current_time.strftime('%H:%M')).split(':')
+        time = time.split(':')
+        if current_time[2] != time[2]:
+            return False
+        elif int(current_time[0]) != int(time[0]):
+            return False
+        elif int(current_time[1]) + buffer <= int(time[1]) and int(current_time[1]) >= int(time[1]) - buffer:
+            return True
+        else:
+            return False
+    def get_day(self):
+        today = datetime.today().weekday()
+        return self.schedule_data[self.days[today]]
+    
     def start_mail_server(self, smtp_server: str, mailer: array, context=None, port: int=None):
         '''
         Starts SMTP server instance via given smtp_server, mailer[email, password], and context
@@ -61,24 +76,30 @@ class Schedule:
             context = ssl.create_default_context()
         if port is None:
             port = 587
-            
-        try:
-            self.server = smtplib.SMTP(smtp_server, port)
-            self.server.ehlo()
-            self.server.starttls(context=context)
-            self.server.ehlo()
-            self.server.login(self.mailer_email, mailer_password)
 
+        try:
+            server = smtplib.SMTP(smtp_server, port)
+            server.ehlo()
+            server.starttls(context=context)
+            server.ehlo()
+            server.login(self.mailer_email, mailer_password)
+            return server
+        
         except Exception as e:
             print(e)
 
-    def end_mail_server(self):
-        self.server.quit()
+    def end_mail_server(server):
+        server.quit()
     
-    def send_notification(self, recipient: str, subject: str, body: str):
+    def send_notification(self, server, recipient: str, subject: str, body: str):
         '''
         '''
         try:
-            self.server.send_message()
+            msg = EmailMessage()
+            msg['Subject'] = subject
+            msg['Message'] = body
+            server.send_message(msg, self.mailer_email, recipient)
+            server.quit()
+          
         except Exception as e:
             print(e)
